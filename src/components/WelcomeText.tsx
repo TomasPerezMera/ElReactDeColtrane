178c4aa453a7567e49fcc9cccbd65539897b1711
Tomás Pérez Mera <tperezmera@gmail.com>
2025-05-14 02:54:12 +0200

    Version 3.1 - Re-added Welcome Image and other commit fixes after an error in commit upload.-

--- src/components/WelcomeText.tsx
+++ src/components/WelcomeText.tsx
@@ -3,21 +3,27 @@ import '../style/style.css';
 
 export default function WelcomeText() {
     return (
-    <div className="welcome">
-        <p data-aos="zoom-in-down" data-aos-duration="1500" data-aos-delay="700">
-            <b>Hola!</b><br></br>Llegaste al Rincón de Coltrane.
-        </p>
-        <div className="separator"></div>
-        <p data-aos="fade-down" data-aos-duration="1500" data-aos-delay="1800">
-            Un pequeño, pero valioso rincón de Internet, donde podrás adquirir algunos de los álbumes más célebres del legendario saxofonista en formato de vinilo.
-        </p>
-        <div className="separator"></div>
-        <p data-aos="fade-down" data-aos-duration="1500" data-aos-delay="3000">
-            A continuación, procederemos a listar los ítems disponibles.<br></br><br></br>Debido al stock limitado, se permite un máximo de 5 vinilos por compra - elegí con cuidado!
-        </p>
-        <Link to="/catalog">
-            <button aria-label="Mostrar Catalogo">Mostrar Catálogo</button>
-        </Link>
+    <div className="welcomeContainer">
+        <div className="welcomeImage">
+            <Link to="/catalog">
+                <img src="./coltrane.jpg" alt="Coltrane Welcome Image"></img>
+            </Link>
+        </div>
+        <div className="welcomeText">
+            <p data-aos="zoom-in-down" data-aos-duration="1500" data-aos-delay="700">
+                <b>Hola!</b><br></br>Llegaste al Rincón de Coltrane.
+            </p>
+            <div className="separator"></div>
+            <p data-aos="fade-down" data-aos-duration="1500" data-aos-delay="1700">
+                Un pequeño, pero valioso rincón de Internet, donde podrás adquirir algunos de los álbumes más célebres del legendario saxofonista en formato de vinilo.
+            </p>
+            <p data-aos="fade-down" data-aos-duration="1500" data-aos-delay="2500">
+                A continuación, procederemos a listar los ítems disponibles.<br></br><br></br>Debido al stock limitado, se permite un máximo de 5 vinilos por compra - elegí con cuidado!
+            </p>
+            <Link to="/catalog">
+                <button aria-label="Mostrar Catalogo">Mostrar Catálogo</button>
+            </Link>
+        </div>
     </div>
     );
 }
